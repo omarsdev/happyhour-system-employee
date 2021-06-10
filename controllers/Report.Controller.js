078a77097ec43476bdea442141f88426c5f25bef
Mwafak Turk <mwaafaak@hotmail.com>
2021-06-09 20:17:26 -0400

    mwafakTest

---
+++ controllers/Report.Controller.js
@@ -0,0 +1,7 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const { con } = require("../db");
+
+exports.getReportById = asyncHandler(async(req, res, next) => {
+    const {}
+});
