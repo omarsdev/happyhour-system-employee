419482f0ac0442a5573db87e7491117028d60163
Ahmad AlZein <ahmadalzein267@gmail.com>
2021-06-10 15:04:25 +0300

    sql fix

--- controllers/Department.Controller.js
+++ controllers/Department.Controller.js
@@ -2,7 +2,18 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const { con } = require("../config/db");
 
-//ahmad commit
 exports.createDepartment = asyncHandler(async(req, res, next) => {
+    const {}
+});
+
+exports.getDepartments = asyncHandler(async(req, res, next) => {
+
+});
+
+exports.getDepartmentbyId = asyncHandler(async(req, res, next) => {
+
+});
+
+exports.updateDepartment = asyncHandler(async(req, res, next) => {
 
 });
--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,96 +1,36 @@
-/* const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const asyncHandler = require("./async");
 const ErrorResponse = require("../utils/errorResponse");
-const User = require("../model/User");
-const Employee = require("../model/Employee");
+const { con } = require("../config/db");
 
-
-exports.protect = asyncHandler(async (req, res, next) => {
-
-  let token;
-
-  if (req.cookies.token) {
-    token = req.cookies.token;
-  }
-
-  //Make Sure Token exists
-  if (!token) {
-    return next(new ErrorResponse("Not authorize to access this route", 401));
-  }
-
-  //if token exists we need to verify
-  try {
-    //Verify Token
-    //1 - extract the payload
-    const decooded = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.user = await User.findById(decooded.id);
-
-    next();
-  } catch (err) {
-    return next(new ErrorResponse("Not authorize to access this route", 401));
-  }
-});
-
-//prodect for Admin
-exports.protectEmployee = asyncHandler(async (req, res, next) => {
-  //take token
-  let token;
-  //check Headers autherization
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  } else if (req.cookies.token) {
-    token = req.cookies.token;
-  }
-
-  //Make Sure Token exists
-  if (!token) {
-    return next(new ErrorResponse("Not authorize to access this route", 401));
-  }
-
-  try {
-    //Verify Token
-    //1 - extract the payload
-    const vertoken = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.employee = await Employee.findById(vertoken.id);
-
-    next();
-  } catch (err) {
-    return next(new ErrorResponse("Not authorize to access this route", 401));
-  }
-});
-
-//User access to apecific roles
-exports.authorize = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new ErrorResponse(
-          `User Role ${req.user.role} is unautherized to access this routes`,
-          401
-        )
-      );
+exports.protectEmployee = asyncHandler(async(req, res, next) => {
+    //take token
+    let token;
+    //check Headers autherization
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1];
+    } else if (req.cookies.token) {
+        token = req.cookies.token;
     }
-    next();
-  };
-};
 
-//Employee access to apecific roles
-exports.authorizeEmployee = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.employee.role)) {
-      return next(
-        new ErrorResponse(
-          `Employee Role ${req.user.role} is unautherized to access this routes`,
-          401
-        )
-      );
+    //Make Sure Token exists
+    if (!token) {
+        return next(new ErrorResponse("Not authorize to access this route", 401));
     }
-    next();
-  };
-};
- */
+
+    try {
+        //Verify Token
+        //1 - extract the payload
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        //req.employee = await Employee.findById(decoded.id);
+        req.employee = await con.query("SELECT * FROM employee WHERE id = $1", [decoded.id]);
+
+        next();
+    } catch (err) {
+        return next(new ErrorResponse("Not authorize to access this route", 401));
+    }
+});
