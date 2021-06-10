8d3d7675cbc4b7bc20a8deae2f97ef2a6a6b3bb0
Ahmad AlZein <ahmadalzein267@gmail.com>
2021-06-10 11:07:22 +0300

    ahmad

--- controllers/Department.Controller.js
+++ controllers/Department.Controller.js
@@ -2,6 +2,7 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const { con } = require("../config/db");
 
+//ahmad commit
 exports.createDepartment = asyncHandler(async(req, res, next) => {
 
 });
