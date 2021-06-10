84b582fd6467069ca7affa8af654525f8663bb49
Ahmad AlZein <ahmadalzein267@gmail.com>
2021-06-10 15:06:50 +0300

    db

--- config/db.js
+++ config/db.js
@@ -1,21 +1,13 @@
-const {con} = require("mysql");
+const { con } = require("mysql");
 
 const con = mysql.createConnection({
-<<<<<<< HEAD
     connectionLimit: 10,
     host: "127.0.0.1",
     port: 3306,
     user: "root",
     password: "",
     database: "happy_hour",
-=======
-  connectionLimit: 10,
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "happy_hour",
->>>>>>> 52e97ddf2e5df93f80fe406a74081f0820ce0e3d
+
 });
 
 /* function Query(query) {
